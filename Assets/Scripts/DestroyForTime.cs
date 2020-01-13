c7b2871255e7aad351ff9dd129329f3997f15710
Ivan <gusevskif1@gmail.com>
2020-01-13 11:53:11 +0300

    Finalized Bullet

---
+++ Assets/Scripts/DestroyForTime.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyForTime : MonoBehaviour
+{
+    public float time = 0.4f;
+    void Start()
+    {
+        Destroy(gameObject, time);
+    }
+}
